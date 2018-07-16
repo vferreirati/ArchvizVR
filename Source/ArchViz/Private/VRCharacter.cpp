35bf1f1f4039a1474867e5ed24ea63975d41c18d
Victor Ferreira <vferreirati@gmail.com>
2018-07-16 09:54:08 -0300

    VRCharacter class and movement implementation

---
+++ Source/ArchViz/Private/VRCharacter.cpp
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "VRCharacter.h"
+#include "Camera/CameraComponent.h"
+
+
+// Sets default values
+AVRCharacter::AVRCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->SetupAttachment(GetRootComponent());
+}
+
+// Called when the game starts or when spawned
+void AVRCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AVRCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void AVRCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	// Movement Input Binding
+	PlayerInputComponent->BindAxis("MoveForward", this, &AVRCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &AVRCharacter::MoveRight);
+}
+
+void AVRCharacter::MoveForward(float Value) {
+	AddMovementInput(CameraComp->GetForwardVector(), Value);
+}
+
+void AVRCharacter::MoveRight(float Value) {
+	AddMovementInput(CameraComp->GetRightVector(), Value);
+}
---
+++ Source/ArchViz/Public/VRCharacter.h
@@ -0,0 +1,36 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "VRCharacter.generated.h"
+
+UCLASS()
+class ARCHVIZ_API AVRCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	AVRCharacter();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	class UCameraComponent* CameraComp;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// Movement bind functions
+	void MoveForward(float Value);
+	void MoveRight(float Value);
+	
+};
